6b370990b17137f3bdf5074b7abeea1271918488
ngcccc <guocun1611@163.com>
2020-04-09 21:46:08 +0800

    update

---
+++ spring_boot_gettingstart/src/main/java/wzzf/shuguanAPI/ShuguanAPITest.java
@@ -0,0 +1,42 @@
+package wzzf.shuguanAPI;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.utils.HttpTool;
+
+public class ShuguanAPITest {
+    
+    // public static void main(String[] args) {
+    //     // StringBuffer stb = new StringBuffer();
+    //     //         stb.append("caseNo=20191128N0006")
+    //     //             .append("&typeId=2")
+    //     //             .append("&content=本案件达不到立案条件");
+    //     StringBuffer stb = new StringBuffer();
+    //     stb.append("messageNo=20200331N0001")
+    //         .append("&caseArea=450403")
+    //         .append("&caseTime=2020-03-30 12:35:35")
+    //         .append("&casePlace=案发地点")
+    //         .append("&blat=24.3423432")
+    //         .append("&blog=109.234343")
+    //         .append("&content=案情")
+    //         .append("&taskEndTime=2020-04-07 23:59:59")
+    //         .append("&imgUrl=''");
+    //         // .append("&imgUrl=[{")
+    //         // .append("}]");
+            
+
+    //     String postData = stb.toString();
+        
+    //     String requestUrl = "https://develop.gxcerc.com/wzzf/eventApi/sendCaseInfo.action";
+    //     try {
+    //         System.out.println("开始传输："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    //         String rest = HttpTool.sendPost(postData, requestUrl, "");
+    //         System.out.println("结束传输："+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+    //         System.out.println(rest);
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+}
